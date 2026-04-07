d296de44944cfa99b80f9980b2a4eb596839cb8d
Jon Gunderson <jongund@illinois.edu>
2026-04-07 17:25:27 -0500

    updated zip files

--- build-extensions.sh
+++ build-extensions.sh
@@ -3,9 +3,8 @@ echo "Building extensions.."
 
 version="4.1.4"
 
-zip -r ./docs/dist/ainspector-firefox-$version.zip extension-firefox  -x ".*" -x "__MACOSX"
-zip -r ./docs/dist/ainspector-chrome-$version.zip  extension-chrome   -x ".*" -x "__MACOSX"
-zip -r ./docs/dist/ainspector-edge-$version.zip    extension-edge     -x ".*" -x "__MACOSX"
-# zip -r ./docs/dist/ainspector-opera-$version.zip   extension-opera    -x ".*" -x "__MACOSX"
+zip -r ./docs/dist/ainspector-firefox-$version.zip extension-firefox/*  -x ".*" -x "__MACOSX"
+zip -r ./docs/dist/ainspector-chrome-$version.zip  extension-chrome/*   -x ".*" -x "__MACOSX"
+zip -r ./docs/dist/ainspector-edge-$version.zip    extension-edge/*     -x ".*" -x "__MACOSX"
 crx3 extension-opera -p ../pem/opera-ainspector.pem -o ./docs/dist/ainspector-opera-$version.crx
 
